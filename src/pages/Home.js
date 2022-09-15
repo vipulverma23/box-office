aa622b19e0a6c9659963da80e8a14d583820cc4e
Vipul <vipulverma2311@gmail.com>
2022-09-15 18:04:27 +0530

    created preview cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import MainpageLayout from "../components/MainpageLayout";
 import { apiGet } from "../misc/config";
-
+import ShowGrid from "../components/show/ShowGrid";
+import ActorGrid from "../components/actor/ActorGrid";
 const Home = () => {
   const [input, setInput] = useState("");
   const [results, setResults] = useState(null);
@@ -31,11 +32,11 @@ const Home = () => {
       return <div>No results</div>;
     }
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map((item) => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map((item) => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
