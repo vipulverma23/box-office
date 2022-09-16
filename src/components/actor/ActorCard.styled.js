90aaeb041b2c1e07dd3f93c363eb20397ed7328a
Vipul <vipulverma2311@gmail.com>
2022-09-16 13:24:45 +0530

    installed styled components and created styles for search card

---
+++ src/components/actor/ActorCard.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import { SerachCard } from "../styled";
+
+export const StyledActorCard = styled(SerachCard)`
+  .deathday {
+    margin: 0;
+    margin-top: 15px;
+    font-weight: bold;
+  }
+`;
