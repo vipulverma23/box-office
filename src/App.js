f271adc063bb8b34f82bd3a19168c01c22747b09
Vipul <vipulverma2311@gmail.com>
2022-09-18 02:45:13 +0530

    styling app part-1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
-import Show from "./pages/Show"
+import Show from "./pages/Show";
+import { ThemeProvider } from "styled-components";
+const theme = {
+  mainColors: {
+    blue: "#2400ff",
+    gray: "#c6c6c6",
+    dark: "#353535",
+  },
+};
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/starred" element={<Starred />} />
-      <Route path="/show/:id" element={<Show />} />
-      <Route path="*" element={<div>not found</div>} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+        <Route path="/show/:id" element={<Show />} />
+        <Route path="*" element={<div>not found</div>} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
