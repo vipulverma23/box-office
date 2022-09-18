ffd587ebfc43f168e965e6f49b7ad3a5d93ce929
Vipul <vipulverma2311@gmail.com>
2022-09-18 23:18:49 +0530

    deployed the app

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
